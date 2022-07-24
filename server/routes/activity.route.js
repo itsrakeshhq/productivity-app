f661c0988205adcedd25baaba50e211ec9e40ef8
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2022-07-24 14:03:36 +0530

    Add doc
    Signed-off-by: RakeshPotnuru <rakeshpotnuru321@gmail.com>

--- server/routes/activity.route.js
+++ server/routes/activity.route.js
@@ -7,7 +7,9 @@ const {
 
 const router = express.Router();
 
+/* Creating a route for the get request. */
 router.get("/activities", getActivities);
+/* Creating a route for the post request. */
 router.post("/activity", addActivity);
 
 module.exports = router;
